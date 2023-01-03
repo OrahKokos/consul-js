f1b8f4797b98b9b712eaa0f114ab05a4921bb2cd
OrahKokos <marko.paroski.ns@gmail.com>
2023-01-03 14:51:27 +0000

    build: release additions

---
+++ packages/consul-js-common/release.config.js
@@ -0,0 +1,29 @@
+var name = 'consul-js-common';
+var srcRoot = 'packages/' + name;
+
+module.exports = {
+  extends: 'release.config.base.js',
+  pkgRoot: 'dist/' + srcRoot,
+  tagFormat: name + '-v${version}',
+  commitPaths: [srcRoot + '/*'],
+  plugins: [
+    '@semantic-release/commit-analyzer',
+    '@semantic-release/release-notes-generator',
+    [
+      '@semantic-release/changelog',
+      {
+        changelogFile: srcRoot + '/CHANGELOG.md',
+      },
+    ],
+    '@semantic-release/npm',
+    [
+      '@semantic-release/git',
+      {
+        assets: [srcRoot + '/package.json', srcRoot + '/CHANGELOG.md'],
+        message:
+          'release(version): Release ' + name +
+          '\n${nextRelease.version} [skip ci]\n\n${nextRelease.notes}',
+      },
+    ],
+  ],
+};
